47f21e8898647864f392261b56ff6e3ffee2b0bc
Lecser <Lecser025@gmail.com>
2022-09-11 18:42:02 +0300

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
+
+export const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://6313f1dda8d3f673ffd1f738.mockapi.io/items")
+      .then((r) => r.json())
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(10)].map((_, index) => <Skeleton key={index} />)
+          : items.map((el) => {
+              return <PizzaBlock key={el.id} {...el} />;
+            })}
+      </div>
+    </div>
+  );
+};
